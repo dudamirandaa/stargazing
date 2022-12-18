ae1d4faa3809ffb63d782551cf5c91afe09ea6a6
Eduarda Miranda <eduardamiranda1901@icloud.com>
2022-12-18 17:47:40 -0300

    initial commit

---
+++ stargazing-backend/src/main/java/project/stargazingbackend/StargazingBackendApplication.java
@@ -0,0 +1,13 @@
+package project.stargazingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StargazingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StargazingBackendApplication.class, args);
+	}
+
+}
---
+++ stargazing-backend/src/test/java/project/stargazingbackend/StargazingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package project.stargazingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StargazingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
